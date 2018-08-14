8e85970990a14d5b278fade3598a79ea965bde5a
aakashvarma18 <aakashvarma18@gmail.com>
2018-08-15 02:45:20 +0530

    first package created

---
+++ package/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='package',
+      version='0.1',
+      description='A python testing package',
+      url='https://github.com/aakashvarma/python-pip-package',
+      author='Suspended Hysteria',
+      author_email='suspendedhysteria@gmail.com',
+      license='MIT',
+      packages=['package'],
+      zip_safe=False)
